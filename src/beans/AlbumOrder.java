dffe401118a339b512ba46b9f7fa28a8dec1fa0a
Panicch <37112713+Panicch@users.noreply.github.com>
2020-07-11 01:38:47 +0200

    Beans created

---
+++ src/beans/AlbumOrder.java
@@ -0,0 +1,15 @@
+package beans;
+
+import java.util.Map;
+
+public class AlbumOrder {
+    private Map order;
+
+    public Map getOrder() {
+        return order;
+    }
+
+    public void setOrder(Map order) {
+        this.order = order;
+    }
+}
