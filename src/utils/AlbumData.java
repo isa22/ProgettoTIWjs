18a3421b67b55d5f6fe3a4de911a30098ed1e1ae
Elio <elio.salvini@mail.polimi.it>
2020-07-19 20:01:26 +0200

    Gson response added to detAlbum servlet

---
+++ src/utils/AlbumData.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.util.List;
+
+import beans.Image;
+
+public class AlbumData{
+	private List<Image> images;
+	private String albumTitle;
+	private int albumId;
+	
+	public void AlbumData(List<Image> images, String albumTitle, int albumId) {
+		this.images = images;
+		this.albumTitle = albumTitle;
+		this.albumId = albumId;
+	}
+}
