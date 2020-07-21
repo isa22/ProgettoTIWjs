a29180ab6e9ce92b4d38659d0899a79ac88f96ea
isa22 <37273673+isa22@users.noreply.github.com>
2020-07-22 00:33:07 +0200

    Update Logout.java

--- src/controllers/Logout.java
+++ src/controllers/Logout.java
@@ -23,7 +23,7 @@ public class Logout extends HttpServlet {
 		if (session != null) {
 			session.invalidate();
 		}
-		String path = getServletContext().getContextPath() + "/Home.html";
+		String path = getServletContext().getContextPath() + "/Login.html";
 		response.sendRedirect(path);
 	}
 
