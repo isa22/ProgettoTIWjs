b0319ec114ecaa9b8b772a9f9c0ee8fd29fa32cd
isa22 <37273673+isa22@users.noreply.github.com>
2020-07-23 20:31:31 +0200

    Update Login.java

--- src/controllers/Login.java
+++ src/controllers/Login.java
@@ -52,10 +52,8 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String usrn = null;
 		String pwd = null;
-		System.out.println(request.getParameter("username"));
 		usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
 		pwd = StringEscapeUtils.escapeJava(request.getParameter("password"));
-		System.out.println(usrn);
 		if (usrn == null || pwd == null || usrn.isEmpty() || pwd.isEmpty() ) {
 			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 			response.getWriter().println("Richiesta non valida");
